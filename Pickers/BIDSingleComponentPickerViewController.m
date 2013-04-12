4150b68f8d030e7d6ea8087b7d2ccee3192384aa
lixiaolei19821218 <735534185@qq.com>
2013-04-13 00:07:34 +0800

    On Page 199

--- Pickers/BIDSingleComponentPickerViewController.m
+++ Pickers/BIDSingleComponentPickerViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.characterNames = @[@"Luke", @"Leia", @"Han", @"Chewbacca", @"Artoo", @"Threepio", @"Lando"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +36,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed
+{
+    NSInteger row = [self.singlePicker selectedRowInComponent:0];
+    NSString *selected = self.characterNames[row];
+    NSString *title = [[NSString alloc] initWithFormat:@"You selected %@!", selected];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:@"Thank you for choosing." delegate:nil cancelButtonTitle:@"You're Welcome" otherButtonTitles:nil];
+    [alert show];
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.characterNames count];
+}
 @end
